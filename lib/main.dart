8123f603e1185b4c32888adf47f97b1314b2c3e6
abdelrhman-gmal <120998362+abdelrhman-gmal@users.noreply.github.com>
2023-03-31 01:11:22 +0200

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -3,25 +3,33 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return  MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title:const Text('First Application'),
-          backgroundColor: Colors.indigo[900],
-        ),
-        body: const Center(
-          child: Text(
-            'Abdul Rahman Gamal Fathi',
-            style: TextStyle(fontSize: 20),
+        appBar:AppBar(
+          title: const Text(
+            'Birthday Card',
           ),
         ),
-      ),
+        body:Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Image(image: AssetImage(r'image/pic.jpg')
+            ),
+            Text(
+              'Happy Birthday !!'
+            ),
+          ],
+        ),
+      )
     );
   }
 }
+
